3c273d5733a8f69a0b1b6db3016c9dc2ab58bceb
Rnady <randy.briest@gmail.com>
2020-11-11 08:02:29 -0600

    Added note for future me to modify code grabbing images.  Right now I'm only grabbing png, jpg, or jfif files named 'box-art-front' but I'm going to eventually be changing how I serve up images and don't want to miss the change I made here

--- routes/images.js
+++ routes/images.js
@@ -9,6 +9,7 @@ const express = require('express'),
 router.use(express.static(process.env.GAMES_PATH));
 
 router.get('/list', async (rec, res,) => {
+	// TODO: Remove the hard-coded box-art-front once we're serving up images in a better way
 	// const files = await glob('**/*.{png,jpg,jfif}', {cwd: process.env.GAMES_PATH});
 	const files = await glob('**/box-art-front.{png,jpg,jfif}', {cwd: process.env.GAMES_PATH});
 	res.json(files);
